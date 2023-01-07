e6bc9247cb298bdac28dbde83ce524042b7aa745
menisadi <menisadi@gmail.com>
2023-01-08 00:22:00 +0200

    MS: recreating the pipleline

---
+++ calcualte_states.py
@@ -0,0 +1,14 @@
+def states(quarter, make, value, previous_quarter, previous_make, previous_value):
+    if pd.isna(previous_make) or previous_quarter < quarter:
+        return "START_TO_" + make + str(int(value))
+    return previous_make + str(int(previous_value)) + "_TO_" + make + str(int(value))
+
+def generate_states(df):
+    previous = df[['QUARTER','MAKE_MISS', 'VALUE']].shift(1)
+    previous.columns = ['PREVIOUS_QUARTER','PREVIOUS_MAKE_MISS', 'PREVIOUS_VALUE']
+    df_with_previous = pd.concat([df, previous], axis=1)
+
+    df_states = df_with_previous.apply(lambda x: states(x.QUARTER, x.MAKE_MISS, x.VALUE, x.PREVIOUS_QUARTER, x.PREVIOUS_MAKE_MISS, x.PREVIOUS_VALUE), axis=1)
+    return pd.concat([df, df_states], axis=1)
+    
+    
